269be32c6d2556321b92bd984f23d38ccf175e23
Burak Arslan <burak+github@arskom.com.tr>
2016-09-09 15:51:21 +0300

    Make AmpSDK importable, add license headers to files

--- python/ampdist.py
+++ python/ampdist.py
@@ -1,28 +1,49 @@
-#!/usr/bin/python
-# -*- coding: <utf-8> -*-
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of the Arskom Mobile Platform SDK repository.
+# Copyright (c) Arskom Ltd. All rights reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
 import ConfigParser
 import subprocess
 import sys
 import os
 import tempfile
+import requests
+
+from setuptools import Command
+
 from distutils import dir_util, file_util
 from distutils.spawn import find_executable
-import requests
-from setuptools import Command
+
 try:
     import docker
-except:
+except ImportError:
     pass
 
 try:
     from pkginfo import UnpackedSDist
-except:
+except ImportError:
     pass
 
 
 gen_diff_py = """
-#!/usr/bin/python
-# -*- coding: <utf-8> -*-
+#!/usr/bin/env python
+# encoding: utf-8
 import os
 
 cvol_path = "/export/"
